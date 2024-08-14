ae965307f1ea57185c4b4033d890bd883f24bf3a
aboveStars <yunuskorkmazis384@outlook.com>
2024-08-14 19:23:01 +0300

    minor changes.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Match all API routes
+        source: "/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
