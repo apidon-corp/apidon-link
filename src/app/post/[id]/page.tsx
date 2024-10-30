0c8d97448b894e30275762ff3eca7ac0d0302df0
aboveStars <yunuskorkmazis384@outlook.com>
2024-10-30 15:16:20 +0300

    Production branch created.

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function page() {
   return (
     <div className="w-full h-screen flex flex-auto justify-center items-center">
-      <text className="text-center">You will be directed to Apidon App (Dev) in a few seconds.</text>
+      <text className="text-center">You will be directed to Apidon App (Production) in a few seconds.</text>
     </div>
   );
 }
