93727e9c064b3f69c1f4bcd10ed7654ac54dba95
aboveStars <yunuskorkmazis384@outlook.com>
2024-11-18 12:42:05 +0300

    minor changes.

--- src/app/profile/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function page() {
-  return (
-    <div className="w-full h-screen flex flex-auto justify-center items-center">
-      <text className="text-center">You will be directed to Apidon App (Production) in a few seconds.</text>
-    </div>
-  );
-}
